2db8d9d57dc0c54d8914ceffcd0e10c091d14992
barto <bartlomiej.calko@gmail.com>
2021-12-04 21:02:56 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TicketStar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
