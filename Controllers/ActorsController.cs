f07acd4e65a79504c5398b2641fb773efe278ddd
barto <bartlomiej.calko@gmail.com>
2021-12-05 12:33:42 +0100

    added models and controllers, DbContext, migration and created DB in SQL Server

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using TicketStar.Data;
+
+namespace TicketStar.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
